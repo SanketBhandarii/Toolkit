3912207b67a2908bb30f61f6442cce94f89ed327
SanketBhandarii <bhandarisanketp@gmail.com>
2025-05-06 13:08:50 +0530

    Vercel serverless function maximum size issue troubleshooting

--- src/components/TextToSpeech/utils/getSpeech.ts
+++ src/components/TextToSpeech/utils/getSpeech.ts
@@ -1,37 +1,20 @@
-let tts: any;
-let modelLoading: Promise<any> | null = null;
-
 const audioCache = new Map();
-
-async function loadModel() {
-  if (tts) return tts;
-
-  if (!modelLoading) {
-    modelLoading = import("kokoro-js").then(async ({ KokoroTTS }) => {
-      const model = await KokoroTTS.from_pretrained(
-        "onnx-community/Kokoro-82M-ONNX",
-        { dtype: "q8" }
-      );
-      tts = model;
-      return model;
-    });
-  }
-
-  return modelLoading;
-}
-
 export async function getSpeech(text: string, voice: string) {
   const cacheKey = `${text}_${voice}`;
-  if (audioCache.has(cacheKey)) return audioCache.get(cacheKey);
+  if (audioCache.has(cacheKey)) {
+    return audioCache.get(cacheKey);
+  }
 
-  const tts = await loadModel();
+  const { KokoroTTS } = await import("kokoro-js");
+
+  const model_id = "onnx-community/Kokoro-82M-ONNX";
+  const tts = await KokoroTTS.from_pretrained(model_id, {
+    dtype: "q8",
+  });
 
   const audio = await tts.generate(text, {
     voice: voice === "US-Male" ? "am_adam" : "af_heart",
   });
 
-  audioCache.set(cacheKey, audio);
   return audio;
 }
-
-export { loadModel };
