6bab262d0d8f97a579c565767fbacee5de3a36b2
SanketBhandarii <bhandarisanketp@gmail.com>
2025-05-19 02:12:38 +0530

    Theme updated to dark mode

--- src/components/ImageToText/PreviewImage.tsx
+++ src/components/ImageToText/PreviewImage.tsx
@@ -7,15 +7,15 @@ interface Props {
 
 export default function PreviewImage({ imageUrl, onReset }: Props) {
   return (
-    <div className="flex flex-col items-center gap-4">
+    <div className="flex flex-col gap-4">
       <img
         src={imageUrl}
         alt="Preview"
-        className="max-w-sm rounded-xl shadow-md"
+        className="max-w-xs rounded-xl shadow-md"
       />
       <button
         onClick={onReset}
-        className="text-left text-sm text-red-400 hover:underline cursor-pointer"
+        className="text-left text-sm text-red-300 hover:underline cursor-pointer"
       >
         Remove Image
       </button>
